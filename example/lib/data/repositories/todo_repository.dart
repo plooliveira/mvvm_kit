0e3f11c6b6001a64396ddb4d6241ca37790401da
Paulo Oliveira <pv.oliveira@live.com>
2025-11-13 14:07:02 -0300

    refactor: Update TodoRepository to be abstract and implement in ObjectBoxTodoRepository

--- example/lib/data/repositories/todo_repository.dart
+++ example/lib/data/repositories/todo_repository.dart
@@ -4,17 +4,28 @@ import 'package:mvvm_kit/mvvm_kit.dart';
 import 'package:objectbox/objectbox.dart';
 import '../models/todo_item.dart';
 
-class TodoRepository {
+abstract class TodoRepository {
+  LiveRepositoryData<List<TodoItem>> get todos;
+
+  void add(String title);
+  void toggle(int id);
+  void delete(int id);
+  void deleteCompleted();
+  void dispose();
+}
+
+class ObjectBoxTodoRepository implements TodoRepository {
   late final Box<TodoItem> _box;
   late final MutableRepositoryData<List<TodoItem>> _todosData;
   StreamSubscription<List<TodoItem>>? _subscription;
 
-  TodoRepository(Store db) {
+  ObjectBoxTodoRepository(Store db) {
     _box = db.box<TodoItem>();
     _todosData = MutableRepositoryData(value: []);
     _listenToDatabase();
   }
 
+  @override
   LiveRepositoryData<List<TodoItem>> get todos =>
       LiveRepositoryData(_todosData.source);
 
@@ -29,11 +40,13 @@ class TodoRepository {
         });
   }
 
+  @override
   void add(String title) {
     final todo = TodoItem(title: title);
     _box.put(todo);
   }
 
+  @override
   void toggle(int id) {
     final todo = _box.get(id);
     if (todo != null) {
@@ -42,16 +55,19 @@ class TodoRepository {
     }
   }
 
+  @override
   void delete(int id) {
     _box.remove(id);
   }
 
+  @override
   void deleteCompleted() {
     final completed = _todosData.value.where((t) => t.completed).toList();
     final ids = completed.map((t) => t.id).toList();
     _box.removeMany(ids);
   }
 
+  @override
   void dispose() {
     _subscription?.cancel();
     _todosData.dispose();

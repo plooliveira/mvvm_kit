3a51b852b2360e652e3ca2ee884b9ef86114c9b1
Paulo Oliveira <pv.oliveira@live.com>
2025-11-13 08:11:57 -0300

    refactor: update export from view_widget to view_state

--- package/lib/mvvm_kit.dart
+++ package/lib/mvvm_kit.dart
@@ -2,6 +2,6 @@ export 'src/live_data/hotswap_data.dart';
 export 'src/live_data/repository_data.dart';
 export 'src/widgets/watch_widget.dart';
 export 'src/live_data/live_data.dart';
-export 'src/widgets/view_widget.dart';
+export 'src/widgets/view_state.dart';
 export 'src/scope/scope.dart';
 export "src/viewmodel.dart";
